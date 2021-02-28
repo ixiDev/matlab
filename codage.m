a4afdaadc99e7d42d326a7c1d399d0b37e8b4048
ABDELMAJID ID ALI <feedback.mrzero@gmail.com>
2021-02-28 02:10:02 +0100

    Create codage.m

---
+++ codage.m
@@ -0,0 +1,8 @@
+symbols=['a' 'b' 'c' 'd'];
+prob = [0.5 0.25 0.15 0.1];
+dict = huffmandict(symbols, prob, 1);
+
+fprintf("Symbole \t probabilité \t code Huffman \n")
+for i=1:length(symbols)
+  fprintf("%c \t\t %f \t ",symbols(i),prob(i) ); disp(dict{i});
+endfor
